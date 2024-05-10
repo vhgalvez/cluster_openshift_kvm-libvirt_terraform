37aabb8650c9710423a1bfe0a9175b81bca60cd9
Victor Galvez <vhgalvez@gmail.com>
2024-05-10 17:44:17 +0200

    Update outputs.tf

--- mv_02-cloud-init_cluster_kvm_rockylinux_libvirt_terraform/outputs.tf
+++ mv_02-cloud-init_cluster_kvm_rockylinux_libvirt_terraform/outputs.tf
@@ -1,4 +1,4 @@
 # Output Server IP
 output "vm_ips" {
-  value = { for vm_id, vm in libvirt_domain.vm : vm_id => vm.network_interface[0].addresses }
+  value = { for vm_id, vm in libvirt_domain.vm : vm_id => libvirt_domain.vm[vm_id].network_interface.0.addresses }
 }
