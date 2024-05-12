c2a56271a8ac33d8727461586ba542d98fac25f3
Victor Galvez <vhgalvez@gmail.com>
2024-05-12 02:43:14 +0200

    Update main.tf

--- mv-01-bastion1-cloud-init_kvm_rocky_linux_libvirt/main.tf
+++ mv-01-bastion1-cloud-init_kvm_rocky_linux_libvirt/main.tf
@@ -72,8 +72,10 @@ resource "libvirt_domain" "vm" {
   cloudinit = libvirt_cloudinit_disk.vm_cloudinit[each.key].id
 
   graphics {
-    type = "none" # Headless setup, typically no graphical interface needed
+    type        = "vnc"
+    listen_type = "address"
   }
+
 
   cpu {
     mode = "host-passthrough"
