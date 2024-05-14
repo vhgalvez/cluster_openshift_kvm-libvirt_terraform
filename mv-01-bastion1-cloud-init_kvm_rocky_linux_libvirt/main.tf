9d07f2dfa65b07395806f4930d8037e8314661aa
Victor Galvez <vhgalvez@gmail.com>
2024-05-14 02:23:17 +0200

    Update main.tf

--- mv-01-bastion1-cloud-init_kvm_rocky_linux_libvirt/main.tf
+++ mv-01-bastion1-cloud-init_kvm_rocky_linux_libvirt/main.tf
@@ -28,6 +28,10 @@ data "template_file" "vm_configs" {
     ssh_keys = jsonencode(var.ssh_keys)
     hostname = each.value.hostname
     timezone = var.timezone
+    ipaddr   = each.value.ip
+    gateway  = "192.168.0.1"
+    dns1     = "8.8.8.8"
+    dns2     = "8.8.4.4"
   }
 }
 
